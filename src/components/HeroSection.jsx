b23af457e178f761db4b4bc2d06107a6a18f5724
baditu <baditaandrei20@gmail.com>
2024-01-30 17:37:23 +0200

    Hero Section complete

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,10 +13,7 @@ const HeroSection = () => {
           recipes served with a modern twist.
         </p>
 
-        <button
-          className="bg-text-color-1 font-bold py-2 px-4 rounded-xl text-sm mt-4 hover:bg-light-color-2"
-          onClick={() => console.log("fuck")}
-        >
+        <button className="bg-text-color-1 font-bold py-2 px-4 rounded-xl text-sm mt-4 hover:bg-light-color-2">
           <Link
             className="text-black font-bold"
             key={`link-to-reservation`}
@@ -29,7 +26,7 @@ const HeroSection = () => {
       <img
         src="/images/Food.jpg"
         alt="iamge-for-hero-page"
-        className="rounded-xl w-1/3 max-w-96 max-h-96 hidden md:block border-black border-2 absolute top-10 md:right-10 lg:right-1/5 xl:right-1/4"
+        className="rounded-xl w-1/3 max-w-96 max-h-96 hidden md:block border-black border-2 absolute top-10 md:right-10 lg:right-1/5 xl:right-1/5"
       />
     </article>
   );
