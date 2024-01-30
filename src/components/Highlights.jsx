31cd338900dfa3e495402d039489279575f64cf3
baditu <baditaandrei20@gmail.com>
2024-01-30 18:18:19 +0200

    finish Highlights and Testimonials

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -3,7 +3,7 @@ import { highlights } from "../utils/constants";
 
 const Highlights = () => {
   return (
-    <article className="md:mt-48 p-8 px-16">
+    <article className="md:mt-48 p-8 mx-auto">
       <section className=" flex justify-between content-center mb-6">
         <h2 className="text-black font-bold font-markazi sm:text-3xl">
           This week's specials!
@@ -12,9 +12,11 @@ const Highlights = () => {
           Online Menu
         </button>
       </section>
-      <section className="grid grid-cols-1 sm:grid-cols-3 gap-20">
-        {(highlights ?? []).map((elem) => {
-          return <SpecialCard key={elem.title} specialty={elem} />;
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+        {(highlights ?? []).map((elem, index) => {
+          return (
+            <SpecialCard key={elem.title} specialty={elem} />
+          );
         })}
       </section>
     </article>
